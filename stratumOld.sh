85eb404f3f339beff440bc65db39eca13f322c64
afinieldev <afinieldev@hotmail.com>
2022-08-14 16:33:44 +0200

    Update stratum.sh & clean up

--- stratumOld.sh
+++ stratumOld.sh
@@ -5,48 +5,45 @@
 #####################################################
 
 source /etc/functions.sh
-source $STORAGE_ROOT/yiimp/.yiimp.conf
+source /home/crypto-data/yiimp/.yiimp.conf
 source $HOME/yiimpool/yiimp_single/.wireguard.install.cnf
 
 # Starting the build progress of the stratum
 echo -e "$YELLOW Building blocknotify , iniparser , stratum...$COL_RESET"
 
-cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify
+cd /home/crypto-data/yiimp/yiimp_setup/yiimp/blocknotify
 blckntifypass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 sudo sed -i 's/tu8tu5/'${blckntifypass}'/' blocknotify.cpp
 sudo make -j8
 
 # Compile iniparser , blocknotify and stratum.
-cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
+cd /home/crypto-data/yiimp/yiimp_setup/yiimp/stratum
     git submodule init && git submodule update
     sudo make -C algos
     sudo make -C sha3
     sudo make -C iniparser
     cd secp256k1 && chmod +x autogen.sh && ./autogen.sh && ./configure --enable-experimental --enable-module-ecdh --with-bignum=no --enable-endomorphism && make
-    cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
     if [[ ("$AutoExchange" == "yes") ]]; then
     sudo sed -i 's/CFLAGS += -DNO_EXCHANGE/#CFLAGS += -DNO_EXCHANGE/' $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum/Makefile
 fi
 sudo make -j8
 
-# Copy the compiled stratum files to the correct location.
-echo -e "$YELLOW Building stratum folder structure and copying files...$COL_RESET"
+echo -e " Building stratum folder structure and copying files...$COL_RESET"
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/stratum
 sudo cp -a config.sample/. $STORAGE_ROOT/yiimp/site/stratum/config
 sudo cp -r stratum $STORAGE_ROOT/yiimp/site/stratum
 sudo cp -r run.sh $STORAGE_ROOT/yiimp/site/stratum
+cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+sudo cp -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify/blocknotify $STORAGE_ROOT/yiimp/site/stratum
+sudo cp -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify/blocknotify /usr/bin
 
-cd $STORAGE_ROOT/yiimp/site/stratum
-sudo cp -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify $STORAGE_ROOT/yiimp/site/stratum
-sudo cp -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp/blocknotify /usr/bin/
-
-# Recreate the run.sh file.
 sudo rm -r $STORAGE_ROOT/yiimp/site/stratum/config/run.sh
 echo '#!/usr/bin/env bash
 source /etc/yiimpool.conf
 source $STORAGE_ROOT/yiimp/.yiimp.conf
 ulimit -n 10240
 ulimit -u 10240
-cd '""''"$STORAGE_ROOT"''""'/yiimp/site/stratum
+cd '""''"${STORAGE_ROOT}"''""'/yiimp/site/stratum
 while true; do
 ./stratum config/$1
 sleep 2
@@ -54,26 +51,19 @@ done
 exec bash' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/config/run.sh >/dev/null 2>&1
 sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/config/run.sh
 
-# Remove the old run.sh file, and replace with the new one. And copy the values to ./yiimp file
 sudo rm -r $STORAGE_ROOT/yiimp/site/stratum/run.sh
-
 echo '#!/usr/bin/env bash
 source /etc/yiimpool.conf
 source $STORAGE_ROOT/yiimp/.yiimp.conf
-cd '""''"$STORAGE_ROOT"''""'/yiimp/site/stratum/config/ && ./run.sh $*
+cd '""''"${STORAGE_ROOT}"''""'/yiimp/site/stratum/config/ && ./run.sh $*
 ' | sudo -E tee $STORAGE_ROOT/yiimp/site/stratum/run.sh >/dev/null 2>&1
 sudo chmod +x $STORAGE_ROOT/yiimp/site/stratum/run.sh
 
-# Copy the new values to the DataBase.
 echo -e " Updating stratum config files with database connection info...$COL_RESET"
 cd $STORAGE_ROOT/yiimp/site/stratum/config
 
 sudo sed -i 's/password = tu8tu5/password = '${blckntifypass}'/g' *.conf
 sudo sed -i 's/server = yaamp.com/server = '${StratumURL}'/g' *.conf
-
-# $STORAGE_ROOT
-
-# If wireguard is enabled.
 if [[ ("$wireguard" == "true") ]]; then
   sudo sed -i 's/host = yaampdb/host = '${DBInternalIP}'/g' *.conf
 else
@@ -89,4 +79,3 @@ sudo setfacl -m u:$USER:rwx $STORAGE_ROOT/yiimp/site/stratum/config
 
 echo -e "$GREEN Stratum build complete...$COL_RESET"
 cd $HOME/yiimpool/yiimp_single
-
